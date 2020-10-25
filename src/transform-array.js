4a2177c0c1a9b1045bf7e466c46358e2d3d442ef
Dmitry-Sokolovsky <dmitry635494@gmail.com>
2020-10-25 23:15:33 +0300

    float: fiz array

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,44 +2,31 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transfor(arr) {
 
-  if (!Array.isArray(arr)) {
-    throw Error('Error if the arr is not an Array');
-  }
-
-    let a = [];
-    let rs = true;
-
-  arr.forEach(function(item, i){
-
-    if (item==='--discard-next'){
-      rs = false;
-    } 
-    
-    else if(item==='--discard-prev'){
-      a.pop();
-    } 
-    
-    else if(item==='--double-next'){
-      if(i<arr.length-1){
-        a.push(arr[i+1]);
-      }
-    } 
-    
-    else if(item==='--double-prev'){
-      if(i>0){
-        a.push(arr[i-1]);
-      }
+    if (!Array.isArray(arr)){
+        throw new Error();
     }
-
-      else if(rs){
-        a.push(item);
-      }
-
-      else{
-        rs = true;
-      }
-  });
-
-  return a;
+    const array = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] === '--discard-next') {
+            if(i < (arr.length - 1)) {
+                i++;
+            }
+        } else if(arr[i] === '--discard-prev') {
+            if(i > 0) {
+                array.pop();
+            }            
+        } else if(arr[i] === '--double-next') {
+            if(i < (arr.length - 1)) {
+                array.push(arr[i + 1]);
+            }            
+        } else if(arr[i] === '--double-prev') {
+            if(i > 0) {
+                array.push(arr[i - 1]);
+            }            
+        } else {
+            array.push(arr[i]);
+        }
+    }
+    return array;
 
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,76 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(condition = true) {
+    this.condition = condition;
+  }
+
+  myError(message, key) {
+    if (!message || !key) {
+      throw new Error('Where arguments');
+    }
+  }
+
+  encrypt(message, key) {
+    this.myError(message, key);
+    let encryptedMessage = "";
+    let mLength = message.length;
+    let keyLength = key.length;
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let memory = 0;
+    for (let i = 0; i < mLength; i++) {
+      if (message.codePointAt(i) > 64 && message.codePointAt(i) < 91) {
+        if (memory === keyLength) {
+          memory = 0;
+        }
+        encryptedMessage += String.fromCodePoint(((message.codePointAt(i) + key.codePointAt(memory)) % 26) + 65);
+        memory++;
+      }
+      else {
+        encryptedMessage += message[i];
+      }
+    }
+
+    if (this.condition) {
+      return encryptedMessage;
+    } else {
+      return encryptedMessage.split('').reverse().join('');
+    }
+  }
+
+  decrypt(encryptedMessage, key) {
+    this.myError(encryptedMessage, key);
+    let dcMessage = "";
+    let mLength = encryptedMessage.length;
+    let keyLength = key.length;
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let memory = 0;
+    for (let i = 0; i < mLength; i++) {
+      if (encryptedMessage.codePointAt(i) > 64 && encryptedMessage.codePointAt(i) < 91) {
+        if (memory === keyLength) {
+          memory = 0;
+        }
+        let code = ((encryptedMessage.codePointAt(i) - (key.codePointAt(memory))) % 26);
+        if (code < 0) {
+          code = 26 + code;
+        }
+        dcMessage += String.fromCodePoint(code + 65);
+        memory++;
+      }
+      else {
+        dcMessage += encryptedMessage[i];
+      }
+    }
+
+    if (this.condition) {
+       return dcMessage;
+    } else {
+       return dcMessage.split('').reverse().join('');
+    }
   }
 }
 
