f4edc135060ffaf5361a8121545a4dfaa441d796
Dmitry-Sokolovsky <dmitry635494@gmail.com>
2020-10-25 21:26:25 +0300

    float: extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, opt) {
+    str = String(str);
+  
+    let fank = {
+      repeatTimes: ('repeatTimes' in opt && typeof(opt.repeatTimes) === 'number') ? opt.repeatTimes : 1,
+      separator: 'separator' in opt ? opt.separator : '+',
+      addition: 'addition' in opt ? String(opt.addition) : '',
+      additionRepeatTimes: ('additionRepeatTimes' in opt && typeof(opt.additionRepeatTimes) === 'number') ? opt.additionRepeatTimes : 1,
+      additionSeparator: 'additionSeparator' in opt ? opt.additionSeparator : '|' 
+    };
+  
+    let res = '';
+    if (fank.additionRepeatTimes > 0) {
+      res = fank.addition;
+      for (let i = 1; i < fank.additionRepeatTimes; i++) {
+        res += fank.additionSeparator + fank.addition
+      }
+    }
+    let message = str + res;
+      for (let i = 1; i < fank.repeatTimes; i++) {
+        message += fank.separator + str + res;
+      }
+    
+    return message;
+  };
+
   
